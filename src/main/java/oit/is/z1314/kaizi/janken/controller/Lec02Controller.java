aa7c7d6c406bab26bdaedc106f9fdefc6cda822d
e1b19046 <e1b19046@oit.ac.jp>
2021-10-18 23:01:53 +0900

    ログイン機能を作成

---
+++ src/main/java/oit/is/z1314/kaizi/janken/controller/Lec02Controller.java
@@ -0,0 +1,18 @@
+package oit.is.z1314.kaizi.janken.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.z1314.kaizi.janken.model.Janken;
+
+@Controller
+@RequestMapping("/Lec02")
+public class Lec02Controller {
+}
---
+++ src/main/java/oit/is/z1314/kaizi/janken/model/Janken.java
@@ -0,0 +1,7 @@
+package oit.is.z1314.kaizi.janken.model.Janken
+
+import java.util.ArrayList;
+
+public class Janken {
+
+}

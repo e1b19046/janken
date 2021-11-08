cd62df8eb3d700408442694fbc307622154c6097
e1b19046 <e1b19046@oit.ac.jp>
2021-11-08 16:00:12 +0900

    lec02とlec03の内容を更新

--- src/main/java/oit/is/z1314/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1314/kaizi/janken/controller/Lec02Controller.java
@@ -1,18 +1,60 @@
 package oit.is.z1314.kaizi.janken.controller;
 
 import java.security.Principal;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.method.annotation.ModelAndViewMethodReturnValueHandler;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import oit.is.z1314.kaizi.janken.model.Janken;
+import oit.is.z1314.kaizi.janken.model.Entry;
 
 @Controller
-@RequestMapping("/Lec02")
+@RequestMapping("/lec02")
 public class Lec02Controller {
+  @Autowired
+  private Entry room;
+
+  /**
+   *
+   * @param model
+   * @param prin
+   * @return
+   */
+  @GetMapping
+  public String lec02(ModelMap model, Principal prin) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+    model.addAttribute("login_user", loginUser);
+    return "lec02.html";
+  }
+
+  // @GetMapping
+  // public String lec02Di() {
+  // return "lec02.html";
+  // }
+
+  /**
+   *
+   * @param Hand
+   * @param model
+   * @return
+   */
+  @GetMapping("/{Hand}")
+  public String result(@PathVariable String Hand, ModelMap model) {
+    Janken janken = new Janken();
+    String result = janken.judge(Hand);
+    model.addAttribute("result", result);
+    model.addAttribute("YourHand", janken.ChangeStr(Hand));
+    model.addAttribute("CPUHand", "グー");
+    return "lec02.html";
+  }
 }
